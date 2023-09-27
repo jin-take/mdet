51efc55271ccff8f1361f4a654b737bdad170e95
kona <kitami.kitano20@gmail.com>
2023-09-27 21:42:58 +0900

    :construction: fetchでapi取得のエラー対応中

--- client/app/src/app/page.tsx
+++ client/app/src/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+type Message = {
+  message: string
+}
+
+async function getData() {
+  const res = await fetch("http://127.0.0.1:8080/ping", { cache: 'force-cache' })
+  if (!res.ok) {
+    throw new Error("Failed fetch.")
+  }
+
+  const data = await res.json()
+  return data.message as Message
+}
+
+export default async function Home() {
+  const apiRes = await getData()
+
   return (
     <main className="bg-black">
       <p>MDET APP</p>
+      <p>{apiRes.message}</p>
     </main>
   )
 }
